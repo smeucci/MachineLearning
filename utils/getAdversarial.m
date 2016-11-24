df799a69846b6047df88b4168c5c44f004a97e22
Saverio <s.meucci91@gmail.com>
2016-11-24 14:56:30 +0100

    creating adv example during training

--- utils/getAdversarial.m
+++ utils/getAdversarial.m
@@ -2,10 +2,12 @@ function [adv] = getAdversarial(net, im, label, eps)
 %GETADVERSARIAL Summary of this function goes here
 %   Detailed explanation goes here
 
+net.layers{end} = struct('type', 'softmax') ;
+net = vl_simplenn_tidy(net);
 dzdy = 1;
-res = vl_simplenn_custom(net, im);
+res = vl_simplenn(net, im);
 dzdx = vl_nnsoftmaxloss(res(end).x, label, dzdy);
-res = vl_simplenn_custom(net, im, dzdx, res, 'skipForward', true);
+res = vl_simplenn(net, im, dzdx, res, 'skipForward', true);
 
 adv = res(1).x + eps*sign(res(1).dzdx);
 
