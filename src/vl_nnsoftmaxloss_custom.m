8c2fe11441bb5e4903889e93fc15098cab5e1e51
Saverio <s.meucci91@gmail.com>
2016-11-25 15:48:38 +0100

    fix custom loss

--- src/vl_nnsoftmaxloss_custom.m
+++ src/vl_nnsoftmaxloss_custom.m
@@ -6,8 +6,10 @@ Y = vl_nnsoftmax(res(i).x);
 dY = vl_nnsoftmaxloss(Y, class, 1);
 adv_res = vl_simplenn(net, res(1).x, dY, res, 'skipForward', true);
 adv_res(1).x = res(1).x + eps*sign(adv_res(1).dzdx);
+net.layers{end} = struct('type', 'softmax') ;
+net = vl_simplenn_tidy(net);
 adv_res = vl_simplenn(net, adv_res(1).x);
 
-result = alfa*vl_nnsoftmaxloss(res(i).x, class) + (1 - alfa)*adv_res(i+1).x;
+result = alfa*vl_nnsoftmaxloss(res(i).x, class) + (1 - alfa)*vl_nnsoftmaxloss(adv_res(i).x, class);
 
 end
