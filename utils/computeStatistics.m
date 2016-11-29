a1cb03208b6dbaf8306c075ea6571bfb8f8ca8cf
Saverio <s.meucci91@gmail.com>
2016-11-29 13:49:49 +0100

    new stats for prediction scores

--- utils/computeStatistics.m
+++ utils/computeStatistics.m
@@ -1,4 +1,5 @@
-function [correct, error, meanScore] = computeStatistics(labels, predictedLabels, predictionScores)
+function [correct, error, meanScore, meanCorrectScore, meanNonCorrectScore] = computeStatistics(labels, predictedLabels, ...
+            predictionScores, correctlyPredictionScores, nonCorrectlyPredictionScores)
 %COMPUTESTATISTICS Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -7,6 +8,7 @@ diff = predictedLabels == labels;
 correct = sum(diff == 1) / size(predictedLabels, 2);
 error = sum(diff == 0) / size(predictedLabels, 2);
 meanScore = mean(predictionScores);
+meanCorrectScore = mean(correctlyPredictionScores);
+meanNonCorrectScore = mean(nonCorrectlyPredictionScores);
 
 end
-
